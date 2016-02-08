e17448ec46a4ddf39d299f6686280d3dec07c823
Sal Espinosa <sespinos@gmail.com>
2016-02-08 13:41:35 -0700

    Initial commit for bst

---
+++ bst_test.rb
@@ -0,0 +1,118 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../bin/bst.rb'
+
+class BSTTest < Minitest::Test
+  def test_it_inserts_a_new_node_when_passed_in_data
+    skip
+    tree = BinarySearchTree.new
+
+    assert_equal 0, tree.insert(61, "Bill & Ted's Excellent Adventure")
+    assert_equal 1, tree.insert(16, "Johnny English")
+    assert_equal 1, tree.insert(92, "Sharknado 3")
+    assert_equal 2, tree.insert(50, "Hannibal Buress: Animal Furnace")
+  end
+
+  def test_it_verifies_and_rejects_the_presence_of_a_score
+    skip
+    tree = BinarySearchTree.new
+
+    assert_equal 0, tree.insert(61, "Bill & Ted's Excellent Adventure")
+    assert_equal 1, tree.insert(16, "Johnny English")
+    assert_equal 1, tree.insert(92, "Sharknado 3")
+    assert_equal 2, tree.insert(50, "Hannibal Buress: Animal Furnace")
+
+    assert tree.include?(16)
+    refute tree.include?(72)
+  end
+
+  def test_reports_the_depth_of_the_tree_where_a_score_appears
+    skip
+    tree = BinarySearchTree.new
+
+    assert_equal 0, tree.insert(61, "Bill & Ted's Excellent Adventure")
+    assert_equal 1, tree.insert(16, "Johnny English")
+    assert_equal 1, tree.insert(92, "Sharknado 3")
+    assert_equal 2, tree.insert(50, "Hannibal Buress: Animal Furnace")
+
+    assert_equal 1, tree.depth_of(92)
+    assert_equal 2, tree.depth_of(50)
+  end
+
+  def test_reports_the_move_and_score_of_the_highest_scored_movie
+    skip
+    tree = BinarySearchTree.new
+
+    assert_equal 0, tree.insert(61, "Bill & Ted's Excellent Adventure")
+    assert_equal 1, tree.insert(16, "Johnny English")
+    assert_equal 1, tree.insert(92, "Sharknado 3")
+    assert_equal 2, tree.insert(50, "Hannibal Buress: Animal Furnace")
+
+    expected = {"Sharknado 3"=>92}
+
+    assert_equal expected, tree.max
+  end
+
+  def test_reports_the_move_and_score_of_the_highest_scored_movie
+    skip
+    tree = BinarySearchTree.new
+
+    assert_equal 0, tree.insert(61, "Bill & Ted's Excellent Adventure")
+    assert_equal 1, tree.insert(16, "Johnny English")
+    assert_equal 1, tree.insert(92, "Sharknado 3")
+    assert_equal 2, tree.insert(50, "Hannibal Buress: Animal Furnace")
+
+    expected = {"Johnny English"=>16}
+
+    assert_equal expected, tree.min
+  end
+
+  def test_returns_array_of_all_movies_and_scores_sorted_in_ascending_order
+    skip
+    tree = BinarySearchTree.new
+
+    assert_equal 0, tree.insert(61, "Bill & Ted's Excellent Adventure")
+    assert_equal 1, tree.insert(16, "Johnny English")
+    assert_equal 1, tree.insert(92, "Sharknado 3")
+    assert_equal 2, tree.insert(50, "Hannibal Buress: Animal Furnace")
+
+    expected = [{"Johnny English"=>16},
+    {"Hannibal Buress: Animal Furnace"=>50},
+    {"Bill & Ted's Excellent Adventure"=>61},
+    {"Sharknado 3"=>92}]
+
+    assert_equal expected, tree.sort
+  end
+
+  def test_loads_new_movies_from_file
+    skip
+    tree = BinarySearchTree.new
+
+    assert_equal 0, tree.insert(61, "Bill & Ted's Excellent Adventure")
+    assert_equal 1, tree.insert(16, "Johnny English")
+    assert_equal 1, tree.insert(92, "Sharknado 3")
+    assert_equal 2, tree.insert(50, "Hannibal Buress: Animal Furnace")
+
+    tree.load('../bin/movies.txt')
+
+    assert_equal 26, tree.load('movies.txt')
+  end
+
+  def test_reports_on_health_of_tree
+    skip
+    tree = BinarySearchTree.new
+
+    tree.insert(98, "Animals United")
+    tree.insert(58, "Armageddon")
+    tree.insert(36, "Bill & Ted's Bogus Journey")
+    tree.insert(93, "Bill & Ted's Excellent Adventure")
+    tree.insert(86, "Charlie's Angels")
+    tree.insert(38, "Charlie's Country")
+    tree.insert(69, "Collateral Damage")
+
+    assert_equal [[98, 7, 100]], tree.health(0)
+    assert_equal [[58, 6, 85]], tree.health(1)
+    assert_equal [[36, 2, 28], [93, 3, 42]], tree.health(2)
+  end
+end
